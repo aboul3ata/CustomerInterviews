fdb35ec08e2357e579ca8b1132cc658de38733af
Ali Abouelatta <ata311@nyu.edu>
2021-04-27 00:28:25 -0400

    5.2 part 1

--- backend/src/ref/routes.js
+++ backend/src/ref/routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const verifier = require('firebase-token-verifier');
+const AWS = require('aws-sdk');
+//change
+const USERS_TABLE = "interviews";
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const routes = express.Router({
     mergeParams: true
@@ -20,6 +24,30 @@ routes.get('/', (req, res) => {
     // res.status(200).json({"ref_code":"1234","count":4});
 
 });
+
+routes.get('/test', function (req, res) {
+      const params = {
+        TableName: USERS_TABLE,
+        Key: {
+            interviewId: "test",
+            userId:"usertest"
+        },
+      }
+    
+      dynamoDb.get(params, (error, result) => {
+        if (error) {
+          console.log(error);
+          res.status(400).json({ error: 'Could not get user' });
+        }
+        if (result.Item) {
+          const {userId, name} = result.Item;
+          res.json({ userId, name });
+        } else {
+     res.status(404).json({ error: "User not found" });
+   }
+    });
+})
+
 
 module.exports = {
     routes,
