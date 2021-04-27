9e2025e7a1aa9a23961bb703c8584c843ab234f2
Ali Abouelatta <ata311@nyu.edu>
2021-04-27 03:33:28 -0400

    hw5 complete

--- backend/src/ref/routes.js
+++ backend/src/ref/routes.js
@@ -25,29 +25,73 @@ routes.get('/', (req, res) => {
 
 });
 
-routes.get('/test', function (req, res) {
-      const params = {
-        TableName: USERS_TABLE,
-        Key: {
-            interviewId: "test",
-            userId:"usertest"
-        },
-      }
-    
-      dynamoDb.get(params, (error, result) => {
-        if (error) {
-          console.log(error);
-          res.status(400).json({ error: 'Could not get user' });
-        }
-        if (result.Item) {
-          const {userId, name} = result.Item;
-          res.json({ userId, name });
-        } else {
-     res.status(404).json({ error: "User not found" });
-   }
-    });
+routes.post('/test', function (req, res) {
+
+    if (!req.header("Authorization")){
+        res.status(401).json({});
+    } else{
+        verifier.validate(req.header("Authorization"),"interviews-4b797",(err,validated)=>{
+            if (err) {
+                res.status(401).json({});
+            } else {
+                var params = {
+                    TableName:USERS_TABLE,
+                    Item:{
+                        "userId": req.body.token,
+                        "interviewId": String(Math.floor(Math.random() * 1000000)),
+                        "who":req.body.who,
+                        "web":req.body.web,
+                        "calendar":req.body.calendar
+                    }
+                };
+                dynamoDb.put(params, function(err, data) {
+                    if (err) {
+                        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                        res.status(401).json({});
+                    } else {
+                        console.log("Added item:", JSON.stringify(data, null, 2));
+                        res.status(201).json({});
+                    }
+                });
+            }
+        })
+    }
+
+
 })
 
+routes.get('/test', function (req, res) {
+
+    if (!req.header("Authorization")){
+        res.status(401).json({});
+    } else{
+        verifier.validate(req.header("Authorization"),"interviews-4b797",(err,validated)=>{
+            if (err) {
+                res.status(401).json({});
+            } else {
+                var params = {
+                    TableName : USERS_TABLE,
+                    KeyConditionExpression: "userId = :idz",
+                    ExpressionAttributeValues: {
+                        ":idz": req.header("Authorization")
+                    }
+                };
+            
+                dynamoDb.query(params, (error, result) => {
+                    if (error) {
+                    console.log(error);
+                    res.status(400).json({ error2: 'Could not get user' });
+                    }
+                    console.log(result.Items)
+                    res.status(201).json({"meetings":result.Items});
+
+                });
+            }
+        })
+    }
+
+
+})
 
 module.exports = {
     routes,
